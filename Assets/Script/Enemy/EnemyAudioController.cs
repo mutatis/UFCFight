ca221f8bcef4e2b2418340be2d403a15e2962ccf
Gabriel <mutatisguga@gmail.com>
2015-08-03 14:35:58 -0300

    Som

---
+++ Assets/Script/Enemy/EnemyAudioController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAudioController : MonoBehaviour
+{
+    public AudioClip takedown;
+
+    bool tocaT;
+
+    public void Takedown()
+    {
+        if (!tocaT)
+        {
+            AudioSource.PlayClipAtPoint(takedown, transform.position, 1f);
+            tocaT = true;
+        }
+    }
+}
---
+++ Assets/Script/Player/PlayerAudioController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAudioController : MonoBehaviour
+{
+    public AudioClip soco;
+    public AudioClip socoF;
+
+    //som de soco
+    public void Soco()
+    {
+        AudioSource.PlayClipAtPoint(soco, transform.position, 1f);
+    }
+
+    //som de socoForte
+    public void SocoForte()
+    {
+        AudioSource.PlayClipAtPoint(socoF, transform.position, 1f);
+    }
+}
