2c217ccdd7063c7e637a2ce4fc12e0eefe28ff0d
Gabriel <mutatisguga@gmail.com>
2015-08-04 15:57:17 -0300

    Audio button

---
+++ Assets/SomBotao.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomBotao : MonoBehaviour
+{
+    public void Click(AudioClip click)
+    {
+        AudioSource.PlayClipAtPoint(click, transform.position, 1f);
+    }
+}
