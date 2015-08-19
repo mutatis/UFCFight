578b8c1e7ff27fb6f1c29a5900068c1a9d0cb447
Gabriel <mutatisguga@gmail.com>
2015-08-19 11:45:02 -0300

    Backgrond

---
+++ Assets/Script/Manager/CriaCriador.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CriaCriador : MonoBehaviour 
+{
+	public SpriteRowCreator creator;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			creator.CreateSprites();
+		}
+	}
+}
