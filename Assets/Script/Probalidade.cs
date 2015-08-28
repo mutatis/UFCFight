413bbc8808e4eea8dd2b9b2c4a51d383c397e820
Gabriel <mutatisguga@gmail.com>
2015-08-28 17:00:23 -0300

    f

---
+++ Assets/Script/Probalidade.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Probalidade : MonoBehaviour
+{
+
+    public Gift[] gifts;
+    int giftIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    int ChooseItem()
+    {
+        float total = 0;
+        int i = 0;
+        foreach (Gift elem in gifts)
+        {
+            total += elem.probability;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (i = 0; i < gifts.Length; i++)
+        {
+            if (randomPoint < gifts[i].probability)
+                return i;
+            else
+                randomPoint -= gifts[i].probability;
+        }
+
+        return gifts.Length - 1;
+    }
+}
+
+[System.Serializable]
+public class Gift
+{
+    public GameObject item;
+    public float probability;
+}
