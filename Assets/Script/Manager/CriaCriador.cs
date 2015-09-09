5f8d61b7c8fd0be24f4b0399415e190ee3764b20
Gabriel <mutatisguga@gmail.com>
2015-09-09 11:02:35 -0300

    Bug fixes and Img organization

--- Assets/Script/Manager/CriaCriador.cs
+++ Assets/Script/Manager/CriaCriador.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class CriaCriador : MonoBehaviour 
 {
-    public GameObject enemy;
+    public SpriteRowCreator creator;
 
-	public void Destroy ()
-	{
-        Destroy(enemy);
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //se bateu no player cria sprite com outros inimigos
+        if (other.gameObject.tag == "Player")
+        {
+            creator.CreateSprites();
+        }
+    }
 }
