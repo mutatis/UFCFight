4794494afbac245a655ba4e7616a55736a18112e
Gabriel <mutatisguga@gmail.com>
2015-09-22 16:42:56 -0300

    f

--- Assets/Script/Manager/GameMaster.cs
+++ Assets/Script/Manager/GameMaster.cs
@@ -18,13 +18,13 @@ public class GameMaster : MonoBehaviour
 
     void Update()
     {
-        if (Time.timeScale == 0)
+        if (Time.timeScale == 0 && audio.volume > 0.3f)
         {
-            audio.volume = 0.3f;
+            audio.volume -= 0.01f;
         }
-        else
+        else if(Time.timeScale == 1 && audio.volume < 1)
         {
-            audio.volume = 1;
+            audio.volume += 0.01f;
         }
     }
 }
